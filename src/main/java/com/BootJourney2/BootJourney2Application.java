0255b5323b2dcad2e97193ffd1f86789133632b0
donghwan <kdhe9797@naver.com>
2025-02-27 09:56:10 +0900

    ads

---
+++ src/main/java/com/BootJourney2/BootJourney2Application.java
@@ -0,0 +1,13 @@
+package com.BootJourney2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJourney2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJourney2Application.class, args);
+	}
+
+}
